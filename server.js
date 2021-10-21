45c3cbbf3b0e47caf72403451a7f3971c66140ff
komputerkerja <komputerkerja100@gmail.com>
2021-10-21 08:11:47 +0700

    heroku app update

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
-
+require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.static('public'));
+
+app.get('/api', (req,res) => {
+    res.json({
+        message: process.env.API_BASE_URL
+    });
+})
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
